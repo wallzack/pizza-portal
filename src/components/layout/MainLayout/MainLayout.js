3929cf55fe20adc5e210dadfe64e1e055d4e98bd
wallzack <vulcheek@gmail.com>
2020-03-07 17:39:23 +0100

    18.4 Added Material-UI initial code to AppBar

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PageNav from '../PageNav/PageNav';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
 
-class MainLayout extends React.Component {
-  static propTypes = {
-    children: PropTypes.node,
-  };
 
-  render() {
-    const { children } = this.props;
+const MainLayout = ({children}) => (
+  <div className="MainLayout">
+    <AppBar>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <PageNav />
+        </Toolbar>
+      </Container>
+    </AppBar>
+    <Container maxWidth="lg">
+      <Toolbar />
+      {children}
+    </Container>
+  </div>
+);
 
-    return (
-      <div>
-        <PageNav />
-        {children}
-      </div>
-    );
-  }
-}
+MainLayout.propTypes = {
+  children: PropTypes.node,
+};
 
 export default MainLayout;
--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,38 +1,40 @@
 import React from 'react';
 import styles from './PageNav.module.scss';
 import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 
-const PageNav = () => {
-  return (
-    <nav className={styles.component}>
-      <div>
-        <NavLink className={styles.link} exact to={`${process.env.PUBLIC_URL}/`} activeClassName="active">
-          Homepage
-        </NavLink>
-      </div>
-      <div>
-        <NavLink className={styles.link} to={`${process.env.PUBLIC_URL}/login`} activeClassName="active">
-          Login
-        </NavLink>
-      </div>
-      <div>
-        <NavLink className={styles.link} to={`${process.env.PUBLIC_URL}/tables`} activeClassName="active">
-          Tables
-        </NavLink>
-      </div>
-      <div>
-        <NavLink className={styles.link} to={`${process.env.PUBLIC_URL}/waiter`} activeClassName="active">
-          Waiter
-        </NavLink>
-      </div>
-      <div>
-        <NavLink className={styles.link} to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName="active">
-          Kitchen
-        </NavLink>
-      </div>
-    </nav>
-  );
-};
+const PageNav = () => (
+  <nav className={styles.component}>
+    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName="active">Dashboard</Button>
+    <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/`} activeClassName="active">Login</Button>
+    {/* <div>
+      <NavLink className={styles.link} exact to={`${process.env.PUBLIC_URL}/`} activeClassName="active">
+        Homepage
+      </NavLink>
+    </div>
+    <div>
+      <NavLink className={styles.link} to={`${process.env.PUBLIC_URL}/login`} activeClassName="active">
+        Login
+      </NavLink>
+    </div>
+    <div>
+      <NavLink className={styles.link} to={`${process.env.PUBLIC_URL}/tables`} activeClassName="active">
+        Tables
+      </NavLink>
+    </div>
+    <div>
+      <NavLink className={styles.link} to={`${process.env.PUBLIC_URL}/waiter`} activeClassName="active">
+        Waiter
+      </NavLink>
+    </div>
+    <div>
+      <NavLink className={styles.link} to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName="active">
+        Kitchen
+      </NavLink>
+    </div> */}
+  </nav>
+);
+
 
 export default PageNav;
