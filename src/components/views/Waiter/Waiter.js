9d0527aef5713ee9ee7a74c06dd206c33e0919f2
wallzack <vulcheek@gmail.com>
2020-03-11 20:07:54 +0100

    19.2 bug: cannot read 'active' of undefined

--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Waiter.module.scss';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -9,89 +10,123 @@ import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
-const demoContent = [
-  { id: '1', status: 'free', order: null },
-  { id: '2', status: 'thinking', order: null },
-  { id: '3', status: 'ordered', order: 123 },
-  { id: '4', status: 'prepared', order: 234 },
-  { id: '5', status: 'delivered', order: 345 },
-  { id: '6', status: 'paid', order: 456 },
-];
+// const demoContent = [
+//   { id: '1', status: 'free', order: null },
+//   { id: '2', status: 'thinking', order: null },
+//   { id: '3', status: 'ordered', order: 123 },
+//   { id: '4', status: 'prepared', order: 234 },
+//   { id: '5', status: 'delivered', order: 345 },
+//   { id: '6', status: 'paid', order: 456 },
+// ];
 
-const renderActions = status => {
-  switch (status) {
-    case 'free':
-      return (
-        <div className={styles.buttons}>
-          <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/new`}>
-            Thinking
-          </Button>
+class Waiter extends React.Component {
+  static propTypes = {
+    fetchTables: PropTypes.func,
+    loading: PropTypes.shape({
+      active: PropTypes.bool,
+      error: PropTypes.oneOf([PropTypes.bool,PropTypes.string]),
+    }),
+    tables: PropTypes.object,
+  }
+
+  componentDidMount() {
+    const { fetchTables } = this.props;
+    fetchTables();
+  }
+
+  renderActions(status) {
+    switch (status) {
+      case 'free':
+        return (
+          <div className={styles.buttons}>
+            <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/new`}>
+              Thinking
+            </Button>
+            <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/new`}>
+              New order
+            </Button>
+          </div>
+        );
+      case 'thinking':
+        return (
           <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/new`}>
             New order
           </Button>
-        </div>
-      );
-    case 'thinking':
-      return (
-        <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/new`}>
-          New order
-        </Button>
-      );
-    case 'ordered':
-      return (
-        <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/123abc`}>
-          Prepared
-        </Button>
-      );
-    case 'prepared':
-      return (
-        <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/123abc`}>
-          Delivered
-        </Button>
-      );
-    case 'delivered':
-      return (
-        <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/123abc`}>
-          Paid
-        </Button>
-      );
-    case 'paid':
-      return (
-        <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/123abc`}>
-          Free
-        </Button>
-      );
-    default:
-      return null;
+        );
+      case 'ordered':
+        return (
+          <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/123abc`}>
+            Prepared
+          </Button>
+        );
+      case 'prepared':
+        return (
+          <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/123abc`}>
+            Delivered
+          </Button>
+        );
+      case 'delivered':
+        return (
+          <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/123abc`}>
+            Paid
+          </Button>
+        );
+      case 'paid':
+        return (
+          <Button className={styles.buttons} component={Link} variant="contained" color="primary" to={`${process.env.PUBLIC_URL}/waiter/order/123abc`}>
+            Free
+          </Button>
+        );
+      default:
+        return null;
+    }
   }
-};
 
-const Waiter = () => (
-  <Paper className={styles.component}>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Table</TableCell>
-          <TableCell>Status</TableCell>
-          <TableCell>Order</TableCell>
-          <TableCell>Action</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {demoContent.map(row => (
-          <TableRow key={row.id}>
-            <TableCell component="th" scope="row">
-              {row.id}
-            </TableCell>
-            <TableCell>{row.status}</TableCell>
-            <TableCell>{row.order && <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>{row.order}</Button>}</TableCell>
-            <TableCell>{renderActions(row.status)}
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </Paper>
-);
+  render() {
+    const { loading: { active, error }, tables } = this.props;
 
+    if(active || !tables.length){
+      return (
+        <Paper className={styles.component}>
+          <p>Loading...</p>
+        </Paper>
+      );
+    } else if(error) {
+      return (
+        <Paper className={styles.component}>
+          <p>Error! Details:</p>
+          <pre>{error}</pre>
+        </Paper>
+      );
+    } else {
+      return (
+        <Paper className={styles.component}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Table</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Order</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tables.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell>{row.status}</TableCell>
+                  <TableCell>{row.order && <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>{row.order}</Button>}</TableCell>
+                  <TableCell>{this.renderActions(row.status)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      );
+    }
+  }
+}
 export default Waiter;
